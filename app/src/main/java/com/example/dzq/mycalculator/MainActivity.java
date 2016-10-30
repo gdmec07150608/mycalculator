a00cf9a64352cd2b7708e5afe48c3bcbd5d7cd18
gdmec07150608 <923240139@qq.com>
2016-10-30 18:41:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/dzq/mycalculator/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.dzq.mycalculator;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+/**
+ * Created by acer-pc on 2016/10/8.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+    public void onCreate(Bundle savadInstanceState){
+        super.onCreate(savadInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
